7191c5d0983af147751df3fde24ef367bfebb73a
Zhou Bo <mikefoxnobel@gmail.com>
2020-02-09 21:43:33 +0800

    Add test framework, and sample test

---
+++ Assets/Editors/Tests/Sample/SampleTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class SampleTest
+    {
+        [Test]
+        public void SampleTestPasses()
+        {
+            // Pass the test.
+            Assert.Pass();
+        }
+    }
+}
